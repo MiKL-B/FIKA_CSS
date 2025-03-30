d44ea857885d94ac224fad1f1f84392ad92e171c
MiKL-B <michaelbecquer@protonmail.com>
2025-03-30 10:45:38 +0200

    add icon button

--- project/src/components/DocsButton.vue
+++ project/src/components/DocsButton.vue
@@ -47,7 +47,7 @@
 &lt;button class=&quot;success outline&quot;&gt;Success&lt;/button&gt;
 &lt;button class=&quot;warning outline&quot;&gt;Warning&lt;/button&gt;
 &lt;button class=&quot;dark outline&quot;&gt;Dark&lt;/button&gt;</code></pre>
-    <!-- <h4>Icon</h4>
+    <h4>Icon</h4>
     <button>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -65,8 +65,35 @@
         <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
       </svg>
       Email
-    </button> -->
-   
+    </button>
+    <pre class="code-example">
+<code>&lt;button&gt;
+  &lt;svg&gt;your svg...&lt;/svg&gt;
+  Email
+&lt;/button&gt;</code></pre>
+    <button>
+      Email
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-mail-icon lucide-mail"
+      >
+        <rect width="20" height="16" x="2" y="4" rx="2" />
+        <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+      </svg>
+    </button>
+    <pre class="code-example">
+<code>&lt;button&gt;
+  Email
+  &lt;svg&gt;your svg...&lt;/svg&gt;
+&lt;/button&gt;</code></pre>
     <h4>Display</h4>
     <button class="w-full">Full width</button>
     <pre
